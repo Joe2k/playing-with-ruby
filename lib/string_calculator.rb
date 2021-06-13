9773afcbff90017c4b0c4cb16d2c9289403ff49c
Joe2k <jonathansamuel2k@gmail.com>
2021-06-13 15:05:36 +0530

    Add method with unit tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,15 @@
 class StringCalculator
+
+  def self.add(input)
+    if input.empty?
+      return 0
+    else
+      inputs = input.split(",").map{ |num| num.to_i }
+      if inputs.length==1
+        return inputs[0]
+      else
+        return inputs[0]+inputs[1]
+      end
+    end
+  end
 end
